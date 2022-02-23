c1f672eb0805d3d93bc96d9cb066c3970766990b
Pham Tan Thanh <tanthanh.pham@nfq.asia>
2022-02-23 17:41:24 +0700

    config mysql and use ajax in ListChoice

--- app/public/index.php
+++ app/public/index.php
@@ -1,12 +1,7 @@
 <?php
-include './php/utils.php';
-
-if (isset($_POST['programingLanguages'])) {
-    setcookie("chooseOption", $_POST['programingLanguages'], time() + (3600), "/");
-    header('location: ./index.php');
-}
-
+    include './php/utils.php';
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -14,12 +9,12 @@ if (isset($_POST['programingLanguages'])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="exercise.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script language="javascript" src="http://code.jquery.com/jquery-2.0.0.min.js"></script>
     <title>Unlock Training</title>
 </head>
 
@@ -39,19 +34,33 @@ if (isset($_POST['programingLanguages'])) {
         <h3>Please choose your favorite programing language?</h3>
         <form action="../index.php" method="post">
             <div class="form-group">
-                <input class="form-check-input" type="radio" value="css" id="css" name="programingLanguages" <?php checkOption('chooseOption','css');?> >
+                <input class="form-check-input" type="radio" value="css" id="css" name="programingLanguages" <?php checkOption('chooseOption', 'css');?> >
                 <label class="form-check-label" for="css">CSS</label><br>
-                <input class="form-check-input" type="radio" value="html" id="html" name="programingLanguages"  <?php checkOption('chooseOption','html');?> >
+                <input class="form-check-input" type="radio" value="html" id="html" name="programingLanguages"  <?php checkOption('chooseOption', 'html');?> >
                 <label class="form-check-label" for="html">HTML</label><br>
-                <input class="form-check-input" type="radio" value="javascript" id="javascript" name="programingLanguages"  <?php checkOption('chooseOption','javascript');?> >
+                <input class="form-check-input" type="radio" value="javascript" id="javascript" name="programingLanguages"  <?php checkOption('chooseOption', 'javascript');?> >
                 <label class="form-check-label" for="javascript">JavaScript</label><br>
-                <input class="form-check-input" type="radio" value="php" id="php" name="programingLanguages"  <?php checkOption('chooseOption','php');?> >
+                <input class="form-check-input" type="radio" value="php" id="php" name="programingLanguages"  <?php checkOption('chooseOption', 'php');?> >
                 <label class="form-check-label" for="php">PHP</label><br>
             </div>
         </form>
     </div>
+
+    <script>
+        $(document).ready(function (e){
+            $('input[type="radio"]').click(function (){
+                $.ajax({
+                    url: './php/ListChoice.php',
+                    type: 'post',
+                    data: {
+                        programingLanguages: $(this).val(),
+                    },
+                    dataType: 'text',
+                    success : function (result){
+                    }
+                });
+            });
+        });
+    </script>
 </body>
 </html>
-<script>
-
-</script>
